ebe33e04f3a6db85a26016942a49c7bfcf449815
RaghaviKannan <ragzagnes@gmail.com>
2023-09-09 14:39:51 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -9,46 +9,57 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("")
 
   const getresult = async (searchTerm) => {
-    const result = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
-    if(result.data.meals){
-      setMeals(result.data.meals)
+    if (!searchTerm) {
+      alert("Enter a food name to Search!!")
     }
-    else{
-      alert(`No meal found with ${searchTerm} - Try something else!!`)
+    else {
+      try {
+        const result = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+        if (result.data.meals) {
+          setMeals(result.data.meals)
+        }
+        else {
+          alert(`No meal found with ${searchTerm} - Try something else!!`)
+        }
+      }
+      catch (error) {
+      console.log(error)
     }
   }
+}
 
-  return (
-    <div className='App'>
-      <div>
-        <h1 className='title'>Meal Finder</h1>
-        <input type='text'
-          placeholder='Search Meal'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button className="search-button" onClick={() => getresult(searchTerm)}>Search</button>
-        <span>Suggested : "chicken", "fish" etc</span>
-      </div>
-      <div className='container' style={{ marginTop: "20px" }}>
-        <div className='row'>
-          {
-            meals.map((item, key) => {
-              return <>
-                <div className="card" style={{ width: "18rem", margin: "20px", paddingTop: '20px' }}>
-                  <img src={item.strMealThumb} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{item.strMeal}</h5>
-                    <a href={item.strYoutube} className="btn btn-primary">Watch video</a>
-                  </div>
+
+return (
+  <div className='App'>
+    <div>
+      <h1 className='title'>Meal Finder</h1>
+      <input type='text'
+        placeholder='Search Meal'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button className="search-button" onClick={() => getresult(searchTerm)}>Search</button>
+      <span>Suggested : "chicken", "fish" etc</span>
+    </div>
+    <div className='container' style={{ marginTop: "20px" }}>
+      <div className='row'>
+        {
+          meals.map((item, key) => {
+            return <>
+              <div className="card" style={{ width: "18rem", margin: "20px", paddingTop: '20px' }}>
+                <img src={item.strMealThumb} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{item.strMeal}</h5>
+                  <a href={item.strYoutube} className="btn btn-primary">Watch video</a>
                 </div>
-              </>
-            })
-          }
-        </div>
+              </div>
+            </>
+          })
+        }
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
