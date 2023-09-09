11929c4367ca706c6b9f39a342ce7f68bca5df6b
RaghaviKannan <ragzagnes@gmail.com>
2023-09-09 14:19:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [meals, setMeals] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const getresult = async (searchTerm) => {
+    const result = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+    setMeals(result.data.meals)
+    console.log(result.data.meals)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <h1 className='title'>Meal Finder</h1>
+        <input type='text'
+          placeholder='Search Meal'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button className="search-button" onClick={() => getresult(searchTerm)}>Search</button>
+        <span>Suggested : "chicken", "fish" etc</span>
+      </div>
+      <div className='container' style={{ marginTop: "20px" }}>
+        <div className='row'>
+          {
+            meals.map((item, key) => {
+              return <>
+                <div className="card" style={{ width: "18rem", margin: "20px", paddingTop: '20px' }}>
+                  <img src={item.strMealThumb} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{item.strMeal}</h5>
+                    <a href={item.strYoutube} className="btn btn-primary">Watch video</a>
+                  </div>
+                </div>
+              </>
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
